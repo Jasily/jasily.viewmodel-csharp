e3d3f5d896b71e2f23d92dc7a6d808dd4eb62309
Cologler <skyoflw@gmail.com>
2022-05-03 12:44:16 +0800

    add PropertySelectorHelper

---
+++ Jasily.ViewModel.Tests/Helpers/TestPropertySelectorHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Jasily.ViewModel.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jasily.ViewModel.Tests.Helpers
+{
+    [TestClass]
+    public class TestPropertySelectorHelper
+    {
+        class X
+        {
+            public Y YP { get; }
+        }
+
+        class Y
+        {
+            public Y YA { get; }
+        }
+
+        [TestMethod]
+        public void TestGetPropertyNames()
+        {
+            var propNames = PropertySelectorHelper.GetPropertyNames<X, Y>(x => x.YP.YA.YA.YA);
+            CollectionAssert.AreEqual(new[] { nameof(X.YP), nameof(Y.YA), nameof(Y.YA), nameof(Y.YA) }, propNames.ToArray());
+        }
+    }
+}
---
+++ Jasily.ViewModel/Helpers/PropertySelectorHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Jasily.ViewModel.Helpers
+{
+    public static class PropertySelectorHelper
+    {
+        /// <summary>
+        /// Gets property names from a <see cref="Expression"/>.
+        /// 
+        /// For x => x.A.B, return "A", "B".
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="propertySelector"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static IEnumerable<string> GetPropertyNames<TModel, TValue>(Expression<Func<TModel, TValue>> propertySelector)
+        {
+            if (propertySelector is null)
+            {
+                throw new ArgumentNullException(nameof(propertySelector));
+            }
+
+            if (propertySelector.Body is MemberExpression me)
+            {
+                var names = new List<string>();
+                VisitPropertyAccess(me, names);
+                names.Reverse();
+                return names;
+            }
+
+            throw new ArgumentException($"{propertySelector} is not property access expression.");
+
+            void VisitPropertyAccess(Expression e, List<string> names)
+            {
+                while (e != null)
+                {
+                    switch (e.NodeType)
+                    {
+                        case ExpressionType.MemberAccess:
+                            var me = (MemberExpression)e;
+                            if (me.Member.MemberType != MemberTypes.Property)
+                                throw new ArgumentException($"{propertySelector} is not property access expression.");
+                            names.Add(me.Member.Name);
+                            e = me.Expression;
+                            break;
+
+                        case ExpressionType.Parameter:
+                            return;
+
+                        default:
+                            throw new ArgumentException($"{propertySelector} is not property access expression.");
+                    }
+                }
+            }
+        }
+    }
+}
