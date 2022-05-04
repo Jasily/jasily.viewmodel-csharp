9f817497bb6445a74b2477a79cb26d32922f28a7
Cologler <skyoflw@gmail.com>
2022-05-05 02:08:37 +0800

    feat: export class PropertiesListener

--- Jasily.ViewModel/Extensions/NotifyPropertyChangedExtensions.Listen.cs
+++ Jasily.ViewModel/Extensions/NotifyPropertyChangedExtensions.Listen.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -35,145 +34,25 @@ namespace Jasily.ViewModel.Extensions
             if (propertySelector is null) throw new ArgumentNullException(nameof(propertySelector));
             if (callback is null) throw new ArgumentNullException(nameof(callback));
 
-            var propNames = PropertySelectorHelper.GetPropertyNames(propertySelector).ToArray();
-            if (propNames.Length == 0)
-                throw new ArgumentException("");
-
-            var listener = new PropertiesListener<TModel>(propNames);
+            var propertyNames = PropertySelectorHelper.GetPropertyNames(propertySelector).ToArray();
+            var listener = new PropertiesListener(propertyNames);
             listener.OnPropertyChanged += v => callback(v);
             listener.Subscribe(model);
             return listener;
         }
 
-        interface IInternalPropertiesListener
-        {
-            void OnPropertyChanged(PropertyListener listener);
-        }
-
-        class PropertiesListener<TModel> : IInternalPropertiesListener, IDisposable
+        public static IDisposable ListenPropertyChanged<TModel>(this TModel model,
+            IEnumerable<string> propertyNames, Action<object> callback)
             where TModel : class, INotifyPropertyChanged
         {
-            private readonly PropertyListener[] _listeners;
+            if (model is null) throw new ArgumentNullException(nameof(model));
+            if (propertyNames is null) throw new ArgumentNullException(nameof(propertyNames));
+            if (callback is null) throw new ArgumentNullException(nameof(callback));
 
-            public PropertiesListener(IEnumerable<string> propertyNames)
-            {
-                _listeners = propertyNames.Select(x => new PropertyListener(x, this)).ToArray();
-            }
-
-            public void Subscribe(TModel obj)
-            {
-                var listeners = _listeners;
-                if (listeners.Length > 0)
-                {
-                    listeners[0].Subscribe(obj);
-                    for (var i = 1; i < listeners.Length; i++)
-                    {
-                        listeners[i].Subscribe(listeners[i - 1].GetPropertyValue());
-                    }
-                }
-            }
-
-            void IInternalPropertiesListener.OnPropertyChanged(PropertyListener listener)
-            {
-                Debug.Assert(listener != null);
-
-                var index = Array.IndexOf(_listeners, listener);
-                Debug.Assert(index >= 0);
-
-                // unsubscribe changed
-                var listeners = _listeners;
-                for (var i = listeners.Length - 1; i > index; i--)
-                {
-                    listeners[i].Unsubscribe();
-                }
-
-                // resubscribe
-                for (var i = index + 1; i < listeners.Length; i++)
-                {
-                    listeners[i].Subscribe(listeners[i - 1].GetPropertyValue());
-                }
-
-                this.OnPropertyChanged?.Invoke(listeners[^1].GetPropertyValue());
-            }
-
-            public void Dispose()
-            {
-                OnPropertyChanged = null;
-                foreach (var listener in _listeners)
-                {
-                    listener.Dispose();
-                }
-            }
-
-            public event Action<object> OnPropertyChanged;
-        }
-
-        class PropertyListener : IDisposable
-        {
-            private readonly string _propertyName;
-            private readonly IInternalPropertiesListener _propertiesListener;
-            private WeakReference<object> _wr;
-
-            public PropertyListener(string propertyName, IInternalPropertiesListener propertiesListener)
-            {
-                this._propertyName = propertyName;
-                this._propertiesListener = propertiesListener;
-            }
-
-            private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
-            {
-                if (e?.PropertyName != _propertyName)
-                    return;
-
-                this._propertiesListener.OnPropertyChanged(this);
-            }
-
-            /// <summary>
-            /// Get property value from subscribed object.
-            /// </summary>
-            /// <returns></returns>
-            public object GetPropertyValue()
-            {
-                if (_wr?.TryGetTarget(out var npc) == true)
-                {
-                    return GetPropertyValueHelper.GetPropertyValue(npc, _propertyName);
-                }
-
-                return null;
-            }
-
-            public void Subscribe(object obj)
-            {
-                if (obj is INotifyPropertyChanged npc)
-                {
-                    npc.PropertyChanged += this.OnPropertyChanged;
-                }
-
-                if (_wr is null)
-                {
-                    _wr = new WeakReference<object>(obj);
-                }
-                else
-                {
-                    _wr.SetTarget(obj);
-                }
-            }
-
-            public void Unsubscribe()
-            {
-                if (_wr?.TryGetTarget(out var o) == true && o is INotifyPropertyChanged npc)
-                {
-                    npc.PropertyChanged -= this.OnPropertyChanged;
-                }
-
-                _wr?.SetTarget(null);
-            }
-
-            public void Dispose()
-            {
-                this.Unsubscribe();
-                _wr = null;
-            }
+            var listener = new PropertiesListener(propertyNames);
+            listener.OnPropertyChanged += v => callback(v);
+            listener.Subscribe(model);
+            return listener;
         }
     }
 }
---
+++ Jasily.ViewModel/Helpers/PropertiesListener.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+using Jasily.ViewModel.Internal;
+
+namespace Jasily.ViewModel.Helpers
+{
+    public class PropertiesListener : PropertiesListener.IInternalPropertiesListener, IDisposable
+    {
+        private readonly PropertyListener[] _listeners;
+
+        public PropertiesListener(IEnumerable<string> propertyNames)
+        {
+            _listeners = propertyNames.Select(x => new PropertyListener(x, this)).ToArray();
+
+            if (this._listeners.Length == 0)
+                throw new ArgumentException("properties is empty");
+        }
+
+        public void Subscribe(object obj)
+        {
+            Debug.Assert(this._listeners.Length > 0);
+
+            var listeners = _listeners;
+            for (var i = 0; i < listeners.Length; i++)
+            {
+                listeners[i].Subscribe(i == 0 ? obj : listeners[i - 1].GetPropertyValue());
+            }
+        }
+
+        public void Unsubscribe()
+        {
+            Debug.Assert(this._listeners.Length > 0);
+
+            foreach (var listener in _listeners.Reverse())
+            {
+                listener.Unsubscribe();
+            }
+        }
+
+        void IInternalPropertiesListener.OnPropertyChanged(PropertyListener listener)
+        {
+            Debug.Assert(listener != null);
+            Debug.Assert(this._listeners.Length > 0);
+
+            var index = Array.IndexOf(_listeners, listener);
+            Debug.Assert(index >= 0);
+
+            // unsubscribe changed
+            var listeners = _listeners;
+            for (var i = listeners.Length - 1; i > index; i--)
+            {
+                listeners[i].Unsubscribe();
+            }
+
+            // resubscribe
+            for (var i = index + 1; i < listeners.Length; i++)
+            {
+                listeners[i].Subscribe(listeners[i - 1].GetPropertyValue());
+            }
+
+            this.OnPropertyChanged?.Invoke(listeners[^1].GetPropertyValue());
+        }
+
+        public void Dispose()
+        {
+            OnPropertyChanged = null;
+            this.Unsubscribe();
+        }
+
+        public event Action<object> OnPropertyChanged;
+
+        interface IInternalPropertiesListener
+        {
+            void OnPropertyChanged(PropertyListener listener);
+        }
+
+        class PropertyListener : IDisposable
+        {
+            private readonly string _propertyName;
+            private readonly IInternalPropertiesListener _propertiesListener;
+            private WeakReference<object> _wr;
+
+            public PropertyListener(string propertyName, IInternalPropertiesListener propertiesListener)
+            {
+                if (propertyName is null) throw new ArgumentNullException(nameof(propertyName));
+
+                this._propertyName = propertyName;
+                this._propertiesListener = propertiesListener;
+            }
+
+            private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                if (e?.PropertyName != _propertyName)
+                    return;
+
+                this._propertiesListener.OnPropertyChanged(this);
+            }
+
+            /// <summary>
+            /// Get property value from subscribed object.
+            /// </summary>
+            /// <returns></returns>
+            public object GetPropertyValue()
+            {
+                if (_wr?.TryGetTarget(out var npc) == true)
+                {
+                    return GetPropertyValueHelper.GetPropertyValue(npc, _propertyName);
+                }
+
+                return null;
+            }
+
+            public void Subscribe(object obj)
+            {
+                if (obj is INotifyPropertyChanged npc)
+                {
+                    npc.PropertyChanged += this.OnPropertyChanged;
+                }
+
+                if (_wr is null)
+                {
+                    _wr = new WeakReference<object>(obj);
+                }
+                else
+                {
+                    _wr.SetTarget(obj);
+                }
+            }
+
+            public void Unsubscribe()
+            {
+                if (_wr?.TryGetTarget(out var o) == true && o is INotifyPropertyChanged npc)
+                {
+                    npc.PropertyChanged -= this.OnPropertyChanged;
+                }
+
+                _wr?.SetTarget(null);
+            }
+
+            public void Dispose()
+            {
+                this.Unsubscribe();
+                _wr = null;
+            }
+        }
+    }
+}
